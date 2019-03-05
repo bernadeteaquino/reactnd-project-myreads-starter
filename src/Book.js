3e50edb7598a41f36779d41767dd06c887d8d41d
Bernadete <beteaquino.jf@gmail.com>
2019-03-04 22:44:06 -0300

    fix: Apresentar autores dos livros

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,7 @@ class Book extends Component {
     return(
       <div className="book">
           <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193 }}>
+          <div className="book-cover">
               <img src={book.imageLinks? book.imageLinks.thumbnail : ''} alt={book.title}/>
           </div>
           <div className="book-shelf-changer">
@@ -21,7 +21,7 @@ class Book extends Component {
           </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
       </div>
     )
   }
