3c647ad86d6f697b877c13dae33e2c5743f31c47
Bernadete <beteaquino.jf@gmail.com>
2019-03-04 22:36:55 -0300

    feat: Realizar chamadas a API de listagem de livros

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState(() => ({
+        books
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ListBooks}/>
+        <Route exact path='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )} />
         <Route exact path="/search" component={SearchBooks}/>
       </div>
     )
